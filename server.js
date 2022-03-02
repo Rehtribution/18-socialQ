3273e8076329aefeb317992e79af94353c9bf079
Reht Cavazos <reht.dev@gmail.com>
2022-03-02 12:54:07 -0600

    initial commit, starting file structure.

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 9001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
